0868ab275b65c0184ad4880b67458419b8ce667d
Noah Addino <leslie@Leslie.local>
2021-10-03 17:20:55 +0200

    up

--- BTC_keypair.py
+++ BTC_keypair.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3
+
+
 
 ### This file contain all the functions to interact from a mnemonic seed to the BTC Protocol
-
 import hmac
 import binascii
-import secrets
 import ecdsa
 import base58
 import sys
-from hashlib import sha512, sha256, new, pbkdf2_hmac
+from hashlib import sha512, sha256, new
 from secrets import randbits
 
 def get_bip32masterkey(seed):
@@ -48,21 +47,21 @@ def get_child_key(master_key, index):
 
 # pywallet openssl private key implementation
 def i2o_ECPublicKey(pubkey, compressed=False):
-    # public keys are 65 bytes long (520 bits)
-    # 0x04 + 32-byte X-coordinate + 32-byte Y-coordinate
-    # 0x00 = point at infinity, 0x02 and 0x03 = compressed, 0x04 = uncompressed
-    # compressed keys: <sign> <x> where <sign> is 0x02 if y is even and 0x03 if y is odd
-    if compressed:
-        if pubkey.point.y() & 1:
-            key = '03' + '%064x' % pubkey.point.x()
-        else:
-            key = '02' + '%064x' % pubkey.point.x()
-    else:
-        key = '04' + \
-              '%064x' % pubkey.point.x() + \
-              '%064x' % pubkey.point.y()
+	# public keys are 65 bytes long (520 bits)
+	# 0x04 + 32-byte X-coordinate + 32-byte Y-coordinate
+	# 0x00 = point at infinity, 0x02 and 0x03 = compressed, 0x04 = uncompressed
+	# compressed keys: <sign> <x> where <sign> is 0x02 if y is even and 0x03 if y is odd
+	if compressed:
+		if pubkey.point.y() & 1:
+			key = '03' + '%064x' % pubkey.point.x()
+		else:
+			key = '02' + '%064x' % pubkey.point.x()
+	else:
+		key = '04' + \
+			  '%064x' % pubkey.point.x() + \
+			  '%064x' % pubkey.point.y()
 
-    return key
+	return key
 
 
 def checksum(data):
@@ -89,30 +88,51 @@ def resize_bin(bin, nbits):
 	return (bin)
 
 
-def main():
-    compressed_xpub = True
-    compressed_wif = True
-    prefix = "00"
-    if (len(sys.argv) == 2):
-    	compressed_xpub = sys.argv[1]
-    if (len(sys.argv) == 3):
-    	compressed_xpub = sys.argv[1]
-    	prefix = str(sys.argv[2])
-    if (len(sys.argv) == 4):
-    	compressed_xpub = sys.argv[1]
-    	prefix = str(sys.argv[2])
-    	compressed_wif = sys.argv[3]
-    entropy = secrets.randbits(256)
-    xpriv = hex(entropy)[2:]
-    if (len(xpriv) < 64):
-        xpriv = resize_bin(xpriv, 64)
-    xpub = get_xpub(xpriv, compressed_xpub)
-    xpub_b58 = base58.b58encode(bytes.fromhex(xpub)).decode()
-    xpriv_b58 = base58.b58encode(bytes.fromhex(xpriv)).decode()
-    xpub_hash = get_xpub_hash(xpub)
-    address = get_addr(xpub, prefix)
-    wif = wallet_import_format(xpriv, "80" ,compressed_wif)
-    print("Compressed_xpub : {}      Prefix : {}       Compressed_wif : {}\n".format(compressed_xpub, prefix, compressed_wif))
-    print("xpriv_hex\t: {}\nxpriv_b58\t: {}\nxpub_hex\t: {}\nxpub_b58\t: {}\nxpub_hash\t: {}\naddress\t\t: {}\nWIF\t\t: {}\n".format(xpriv, xpriv_b58, xpub, xpub_b58, xpub_hash, address, wif))
+def putnbr_base(nb, base):
+	out = ""
+	nbr = nb
+	while (nbr >= base):
+		out =  str(int(nbr % base)) + " " + out
+		nbr = int(nbr // base) 
+	out =  str(int(nbr % base)) + " " + out
+	return(out)
 
+
+
+def main():
+	compressed_xpub = True
+	compressed_wif = True
+	prefix = "00"
+	base = 256
+	if (len(sys.argv) == 2):
+		compressed_xpub = bool(sys.argv[1])
+	if (len(sys.argv) == 3):
+		compressed_xpub = bool(sys.argv[1])
+		prefix = str(sys.argv[2])
+	if (len(sys.argv) == 4):
+		compressed_xpub = bool(sys.argv[1])
+		prefix = str(sys.argv[2])
+		compressed_wif = bool(sys.argv[3])
+	if (len(sys.argv) == 5): 
+		compressed_xpub = bool(sys.argv[1])
+		prefix = str(sys.argv[2])
+		compressed_wif = bool(sys.argv[3])
+		base = int(sys.argv[4])
+	entropy = randbits(256)
+	xpriv = hex(entropy)[2:]
+	if (len(xpriv) < 64):
+		xpriv = resize_bin(xpriv, 64)
+	xpub = get_xpub(xpriv, compressed_xpub)
+	xpub_b58 = base58.b58encode(bytes.fromhex(xpub)).decode()
+	xpriv_b58 = base58.b58encode(bytes.fromhex(xpriv)).decode()
+	xpub_hash = get_xpub_hash(xpub)
+	address = get_addr(xpub, prefix)
+	wif = wallet_import_format(xpriv, "80" ,compressed_wif)
+	print("Compressed_xpub : {}      Prefix : {}       Compressed_wif : {}\n".format(compressed_xpub, prefix, compressed_wif))
+	print("xpriv_hex\t: {}\nxpriv_b58\t: {}\nxpub_hex\t: {}\nxpub_b58\t: {}\nxpub_hash\t: {}\naddress\t\t: {}\nWIF\t\t: {}\n".format(xpriv, xpriv_b58, xpub, xpub_b58, xpub_hash, address, wif))
+	print("=================================")
+	print("xpriv in base ", base, "       :")
+	print(putnbr_base(int(xpriv, 16), base))
+
+	
 main()
